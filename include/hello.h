111075b6a370851701ddb1617d64203be59afe19
xxxt14 <1126441731@qq.com>
2025-09-13 16:08:21 +0800

    使用cmake编译

---
+++ include/hello.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+class Hello {
+
+public:
+    void say_hello() {
+        cout << "Hello, World!" << endl;
+        cout << "Hello, CMake!" << endl;
+    }
+};
---
+++ src/hello.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+
+using namespace std;
+
+main() {
+    Hello hello;
+    hello.say_hello();
+    return 0;
+}
